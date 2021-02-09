61035786db766783bb487ff859c276cbfdcf92d2
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-02-08 16:47:26 -0800

    create a React component

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,5 +2,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 // create a React component
-
+// const App = function() {
+const App = () => {
+    return <div>Hi there!</div>
+}
 // show React component on screen
