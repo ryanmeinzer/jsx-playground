1b00eae94e63a4192aaece6ecfdd25cf779ada50
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-02-08 21:47:25 -0800

    adding variables inside JSX

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -3,12 +3,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 // create a React component
 // const App = function() {
+
+// function getButtonText() {
+//     return 'Click on me!'
+// }
+
 const App = () => {
+    const buttonText = 'Click me'
+
     return (
         <div>
-            <label className="label" for="name">Enter name:</label>
+            <label className="label" htmlFor="name">Enter name:</label>
             <input id="name" type="text" />
-            <button style={{ backgroundColor: 'blue', color: 'white' }}>Submit</button>
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>{buttonText}</button>
         </div>
     )
 }
